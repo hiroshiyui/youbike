7481aac7f21af2cc0597b2042c33e2f4062b8934
Huei-Horng Yo <hiroshiyui@gmail.com>
2013-10-29 19:43:14 +0800

    新增 g0v.json、補上開頭授權宣告

--- youbike.rb
+++ youbike.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+=begin
+  YouBike rental stations data to JOSM XML file format converter
+  Copyright (C) 2013  Huei-Horng Yo
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 require 'csv'
 require 'nokogiri'
 require 'open-uri'
